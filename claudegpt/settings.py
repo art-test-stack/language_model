121dd3667e794dafb5b6527d2bd43e8bade738c5
Arthur Testard <testardarthur@gmail.com>
2024-05-14 19:15:49 +0200

    dataset

--- claudegpt/settings.py
+++ claudegpt/settings.py
@@ -1,1 +1,23 @@
-CONTROL_TOKENS = ['⮜unknown⮞', '⮜padding⮞', '⮜start-of-text⮞', '⮜tab⮞', '⮜new-line⮞', '⮜human⮞', '⮜system⮞', '⮜user⮞', '⮜assistant⮞', '⮜end-of-text⮞']
+from pathlib import Path
+# ------------- DATA -------------
+
+CONTROL_TOKENS = [
+    '⮜unknown⮞', 
+    '⮜padding⮞', 
+    '⮜start-of-text⮞', 
+    '⮜tab⮞', 
+    '⮜new-line⮞', 
+    '⮜human⮞', 
+    '⮜system⮞', 
+    '⮜user⮞', 
+    '⮜assistant⮞', 
+    '⮜end-of-text⮞'
+]
+
+DATA_FOLDER = Path("data")
+OUTPUT_FOLDER = Path("output")
+
+# ------------- TRAIN -------------
+
+NUM_THREADS = 16
+PRETRAINING_VAL_RATIO = 1e-3
