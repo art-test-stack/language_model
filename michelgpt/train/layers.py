6449b43402ec4bd51e31d55e89c84633fe0caf56
Arthur Testard <testardarthur@gmail.com>
2024-07-22 15:31:35 +0200

    remove encoder layers

--- michelgpt/train/layers.py
+++ michelgpt/train/layers.py
@@ -1,54 +1,55 @@
 from michelgpt.settings import *
+from michelgpt.train.module import Module
 
 import math
 import numpy as np
+import matplotlib.pyplot as plt
 import torch.nn as nn
 from torch.nn import functional as F
 
 
-class Module(nn.Module):
-    '''class Module'''
-    def nb_parameters(self) -> int:
-        '''Give the number of parameters of the module'''
-        return sum([np.prod(p.size(), dtype = np.int32) for p in self.parameters()])
-
-    def nb_trainable_parameters(self) -> int:
-        '''Give the number of trainable parameters of the module'''
-        return sum([np.prod(p.size(), dtype = np.int32) for p in self.parameters() if p.requires_grad])
-
-    def nb_non_trainable_parameters(self) -> int:
-        '''Give the number of non-trainable parameters of the module'''
-        return sum([np.prod(p.size(), dtype = np.int32) for p in self.parameters() if not p.requires_grad])
-
-    def summary(self) -> None:
-        '''Summarize the module'''
-        print(f'Number of parameters: {self.nb_parameters():,}')
-        print(f'Number of trainable parameters: {self.nb_trainable_parameters():,}')
-        print(f'Number of non-trainable parameters: {self.nb_non_trainable_parameters():,}')
-
-    def clean_nan(self) -> None:
-        '''Remove NaNs from the module gradients'''
-        for p in self.parameters():
-            if p.grad is not None:
-                torch.nan_to_num(p.grad, nan = 0, posinf = 1e5, neginf = -1e5, out = p.grad)
-
-    def clip_gradient(self, max_norm: float) -> None:
-        '''Clip the module gradients'''
-        nn.utils.clip_grad_norm_(self.parameters(), max_norm)
-
-
-# class Linear(nn.Linear):
-#     def __init__(self, in_features: int, out_features: int, bias: bool = False, device=DEVICE) -> None:
-#         super().__init__(in_features, out_features, bias, device)
-
-
 class Linear(nn.Linear, Module):
     '''Linear layer'''
-    def __init__(self, in_features: int, out_features: int, bias: bool = False, device: torch.device = DEVICE, dtype=None) -> None:
+    def __init__(
+            self, 
+            in_features: int, 
+            out_features: int, 
+            bias: bool = False, 
+            device: torch.device = DEVICE, 
+            dtype=None
+        ) -> None:
         super(Linear, self).__init__(in_features=in_features, out_features=out_features, bias=bias, device=device)
         # TODO: Reparametrize weights and bias here
 
 
+class PositionalEncoding(Module):
+    def __init__(self, dim_model: int = DIM_MODEL, n_pos: int = MAX_CONTEXT) -> None:
+        super().__init__()
+        self.register_buffer('table', self._get_sinusoid_encoding_table(dim_model, n_pos))
+    
+    def _get_sinusoid_encoding_table(self, d_model: int = DIM_MODEL, n_pos: int = MAX_CONTEXT):
+        ''' Sinusoid position encoding table '''
+        pos = torch.arange(n_pos, dtype=torch.float32)
+        i = torch.arange(d_model)
+
+        pos_enc = torch.ger(pos, 1e4 ** (- 2 * (i//2) / d_model))
+
+        pos_enc[:, 0::2] = torch.sin(pos_enc[:, 0::2])
+        pos_enc[:, 1::2] = torch.cos(pos_enc[:, 1::2]) 
+        return pos_enc
+    
+    def plot_table(self):
+        pos_enc_np = self.table.cpu().numpy()
+        plt.imshow(pos_enc_np, cmap='viridis', vmin=-1, vmax=1)
+        plt.colorbar()
+        plt.xlabel("Embedding index")
+        plt.ylabel("Sequence index")
+        plt.title('Sinusoidal Positional Encoding Table')
+        plt.show()
+
+    def forward(self, x):
+        return x + self.table[:,:x.size(2)]
+    
 class AttentionBlock(Module):
     '''Scaled Dot-Product Attention'''
 
@@ -57,7 +58,15 @@ class AttentionBlock(Module):
         self.dropout = nn.Dropout(dropout)
         self.softmax = nn.Softmax(dim=-1)
 
-    def forward(self, q, k, v, d_key: int = DIM_KEY, mask=None, mask_value: int = MASK_VALUE):
+    def forward(
+            self, 
+            q: torch.Tensor, 
+            k: torch.Tensor, 
+            v: torch.Tensor, 
+            d_key: int = DIM_HEAD, 
+            mask=None, 
+            mask_value: int = MASK_VALUE
+        ):
         attention = torch.matmul(q / math.sqrt(d_key), k.transpose(2, 3))
 
         if mask is not None:
@@ -72,46 +81,64 @@ class AttentionBlock(Module):
 class MultiHeadAttention(Module):
     '''Multi-Head Attention module'''
     def __init__(
-            self, dim_model: int = DIM_MODEL, n_heads: int = NUM_HEADS, d_key: int = DIM_KEY, 
-            d_value: int = DIM_VALUE, dropout: float = DROPOUT
+            self, 
+            dim_model: int = DIM_MODEL, 
+            n_heads: int = NUM_HEADS, 
+            d_head: int = DIM_HEAD, 
+            dropout: float = DROPOUT
         ) -> None:
         super().__init__()
-        assert(dim_model == d_key * n_heads, "Dimensions are not correct")
-        self.dims = (n_heads, d_key, d_value)
+        assert(dim_model == d_head * n_heads, "Dimensions are not correct")
+        self.n_heads = n_heads
+        self.d_head = d_head
 
-        self.w_q = Linear(dim_model, d_key * n_heads, bias=False) 
-        self.w_k = Linear(dim_model, d_key * n_heads, bias=False) 
-        self.w_v = Linear(dim_model, d_value * n_heads, bias=False)
+        self.w_q = Linear(dim_model, d_head * n_heads, bias=False) 
+        self.w_k = Linear(dim_model, d_head * n_heads, bias=False) 
+        self.w_v = Linear(dim_model, d_head * n_heads, bias=False)
 
         self.attention = AttentionBlock()
         self.dropout = nn.Dropout(dropout)
         self.layer_norm = nn.LayerNorm(dim_model)
 
-    def forward(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, mask=None):
-        n_heads, d_k, d_v = self.dims
-        batch_size = q.size(0)
-        len_q, len_k, len_v = q.size(1), k.size(1), v.size(1)
+    def forward(self, x: torch.Tensor, mask=None):
+        n_heads = self.n_heads
+        d_head = self.d_head
 
-        res = q
+        batch_size = x.size(0)
+        len_x = x.size(1)
 
-        q = self.w_q(q).view(batch_size, len_q, n_heads, d_k).transpose(1,2)
-        k = self.w_k(k).view(batch_size, len_k, n_heads, d_k).transpose(1,2)
-        v = self.w_v(v).view(batch_size, len_v, n_heads, d_v).transpose(1,2)
+        residual = x
+
+        q = self.w_q(x)
+        k = self.w_k(x)
+        v = self.w_v(x)
+
+        q = x.view(batch_size, len_x, n_heads, d_head).transpose(1,2)
+        k = x.view(batch_size, len_x, n_heads, d_head).transpose(1,2)
+        v = x.view(batch_size, len_x, n_heads, d_head).transpose(1,2)
 
         if mask is not None:
             mask = mask.unsqueeze(1)  
         
-        v, attention = self.attention(q, k, v, mask=mask)
-        v = self.dropout(v.transpose(1, 2).contiguous().view(batch_size, len_v, -1)) 
-        v += res
+        output, attention = self.attention(q, k, v, mask=mask)
 
-        out = self.layer_norm(v)
-        return out, attention
+        if self.training:
+            output = self.dropout(v.transpose(1, 2).contiguous().view(batch_size, d_head, -1)) 
+
+        output += residual
+
+        output = self.layer_norm(v)
+        return output, attention
 
 
 class FeedForwardNet(Module):
     '''Position-Wise Feed Forward Network'''
-    def __init__(self, d_in: int = DIM_MODEL, d_latent: int = DIM_MODEL // 2, dropout: float = DROPOUT) -> None:
+    def __init__(
+            self, 
+            d_in: int = DIM_MODEL, 
+            d_latent: int = DIM_FFN, 
+            dropout: float = DROPOUT
+        ) -> None:
         super().__init__()
         self.layer_1 = Linear(d_in, d_latent, dropout)
         self.activation = nn.ReLU()
@@ -120,48 +147,38 @@ class FeedForwardNet(Module):
         self.layer_norm = nn.LayerNorm(d_in, eps=1e-6)
     
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        res = x
-        x = self.dropout(self.layer_2(self.activation(self.layer_1(x)))) 
-        x += res
-        out = self.layer_norm(x)
+        res = self.layer_2(self.activation(self.layer_1(x)))
+
+        if self.training:
+            res = self.dropout(res)
+            
+        res = res + x
+        out = self.layer_norm(res)
         return out
     
-
-class EncoderLayer(Module):
-    '''Encoder layer'''
-    def __init__(
-            self, dim_model: int = DIM_MODEL, n_heads: int = NUM_HEADS, d_key: int = DIM_KEY, 
-            d_value: int = DIM_VALUE, dropout: float = DROPOUT
-        ) -> None:
-        super().__init__()
-        self.self_attention = MultiHeadAttention(
-            dim_model=dim_model, n_heads=n_heads, d_key=d_key, d_value=d_value, dropout=dropout)
-        self.ffn = FeedForwardNet(d_in=dim_model, d_latent=DIM_MODEL // 2)
-
-    def forward(self, x, self_attention_mask=None):
-        x, enc_attention = self.self_attention(x, x, x, mask=self_attention_mask)
-        out = self.ffn(x)
-        return out, enc_attention
-
 
 class DecoderLayer(Module):
     '''Decoder layer'''
     def __init__(
-            self, dim_model: int = DIM_MODEL, n_heads: int = NUM_HEADS, d_key: int = DIM_KEY, 
-            d_value: int = DIM_VALUE, dropout: float = DROPOUT
+            self, 
+            dim_model: int = DIM_MODEL, 
+            dim_ffn: int = DIM_FFN, 
+            n_heads: int = NUM_HEADS, 
+            d_head: int = DIM_HEAD, 
+            dropout: float = DROPOUT
         ) -> None:
         super().__init__()
         self.self_attention = MultiHeadAttention(
-            dim_model=dim_model, n_heads=n_heads, d_key=d_key, d_value=d_value, dropout=dropout)
-        self.enc_attention = MultiHeadAttention(
-            dim_model=dim_model, n_heads=n_heads, d_key=d_key, d_value=d_value, dropout=dropout)
-        self.ffn = FeedForwardNet(d_in=dim_model, d_latent=DIM_MODEL // 2)
+            dim_model=dim_model, n_heads=n_heads, d_head=d_head, dropout=dropout)
+        
+        self.ffn = FeedForwardNet(d_in=dim_model, d_latent=dim_ffn)
 
-    def forward(self, dec_in, enc_out, self_attention_mask=None, enc_dec_attention_mask=None):
-        dec_out, dec_attention = self.self_attention(dec_in, dec_in, dec_in, mask=self_attention_mask)
-        dec_out, enc_dec_attention = self.enc_attention(dec_out, enc_out, enc_out, mask=enc_dec_attention_mask)
+    def forward(self, x, self_attention_mask=None,):
+        
+        dec_out, dec_attention = self.self_attention(x, mask=self_attention_mask)
+
         dec_out = self.ffn(dec_out)
 
-        return dec_out, dec_attention, enc_dec_attention
+        return dec_out, dec_attention
 
 
--- michelgpt/train/model.py
+++ michelgpt/train/model.py
@@ -1,146 +1,110 @@
 from michelgpt.settings import *
 from michelgpt.train.layers import *
+from michelgpt.train.module import Module
 
-import matplotlib.pyplot as plt
 import torch.nn as nn
-
-
-class Encoder(Module):
-    def __init__(
-            self, d_src_vocab, dim_model: int = DIM_MODEL, n_layers: int = NUM_LAYERS, 
-            n_heads: int = NUM_HEADS, d_key: int = DIM_KEY, d_value: int = DIM_VALUE, 
-            dropout: float = DROPOUT, padding_idx: int = 0, max_content: int = MAX_CONTEXT 
-            ) -> None:
-        super().__init__()
-        # TODO: Will change to customed embedding
-        self.embedding = nn.Embedding(
-            num_embeddings=d_src_vocab, embedding_dim=dim_model, padding_idx=padding_idx)
-        self.pos_enc = PositionalEncoding(dim_model, max_content)
-        self.layers = nn.ModuleList([
-            EncoderLayer(
-                dim_model=dim_model, n_heads=n_heads, d_key=d_key, d_value=d_value, dropout=dropout) 
-            for _ in range(n_layers)]
-        )
-
-    def forward(self, src, src_mask=None, return_attention=False):
-        
-        attention_list = []
-
-        out = self.embedding(src)
-        out = self.pos_enc(out)
-        for layer in self.layers:
-            out, attention = layer(out, self_attention_mask=src_mask)
-            attention_list.append(attention) if return_attention else None
-    
-        return (out, attention) if return_attention else (out,)
 
 
 class Decoder(Module):
     def __init__(
-            self, d_tgt_vocab, dim_model: int = DIM_MODEL, n_layers: int = NUM_LAYERS, 
-            n_heads: int = NUM_HEADS, d_key: int = DIM_KEY, d_value: int = DIM_VALUE, 
-            dropout: float = DROPOUT, padding_idx: int = 0, max_content: int = MAX_CONTEXT
+            self, 
+            vocab_size: int = VOCAB_SIZE,
+            dim_model: int = DIM_MODEL,
+            dim_ffn: int = DIM_FFN,
+            n_layers: int = NUM_LAYERS, 
+            n_heads: int = NUM_HEADS,
+            d_head: int = DIM_HEAD,
+            dropout: float = DROPOUT,
+            padding_idx: int = 0,
+            max_content: int = MAX_CONTEXT
         ) -> None:
         super().__init__()
         # TODO: Will change to customed embedding
         self.embedding = nn.Embedding(
-            num_embeddings=d_tgt_vocab, embedding_dim=dim_model, padding_idx=padding_idx)
+            num_embeddings=vocab_size, 
+            embedding_dim=dim_model, 
+            padding_idx=padding_idx
+        )
         self.pos_enc = PositionalEncoding(dim_model, max_content)
         self.layers = nn.ModuleList([
             DecoderLayer(
-                dim_model=dim_model, n_heads=n_heads, d_key=d_key, d_value=d_value, dropout=dropout)
+                dim_model=dim_model, 
+                dim_ffn=dim_ffn, 
+                n_heads=n_heads, 
+                d_head=d_head, 
+                dropout=dropout
+            )
             for _ in range(n_layers)]
         )
     
-    def forward(self, seq, enc_output, src_mask=None, tgt_mask=None, return_attentions=False):
-        self_attn_list, enc_dec_attn_list = [], []
+    def forward(
+            self, 
+            x: torch.Tensor, 
+            mask=None,
+            return_attentions=False
+        ):
+        self_attn_list = []
         
-        dec = self.embedding(seq)
-        dec_out = self.pos_enc(dec)
+        x = self.embedding(x)
+        output = self.pos_enc(x)
         for layer in self.layers: 
-            dec_out, self_attention, enc_dec_attention = layer(
-                dec_in=dec_out, enc_out=enc_output, self_attention_mask=tgt_mask, enc_dec_attention_mask=src_mask)
+            output, self_attention = layer(x=output, self_attention_mask=mask)
             if return_attentions:
                 self_attn_list.append(self_attention) 
-                enc_dec_attn_list.append(enc_dec_attention)
 
         if return_attentions:
-            return dec_out, self_attn_list, enc_dec_attn_list
+            return output, self_attn_list
         
-        return dec_out,
-
-
-class PositionalEncoding(Module):
-    def __init__(self, dim_model: int = DIM_MODEL, n_pos: int = MAX_CONTEXT) -> None:
-        super().__init__()
-        self.register_buffer('table', self._get_sinusoid_encoding_table(dim_model, n_pos))
-    
-    def _get_sinusoid_encoding_table(self, d_model: int = DIM_MODEL, n_pos: int = MAX_CONTEXT):
-        ''' Sinusoid position encoding table '''
-        pos = torch.arange(n_pos, dtype=torch.float32)
-        i = torch.arange(d_model)
-
-        pos_enc = torch.ger(pos, 1e4 ** (- 2 * (i//2) / d_model))
-
-        pos_enc[:, 0::2] = torch.sin(pos_enc[:, 0::2])
-        pos_enc[:, 1::2] = torch.cos(pos_enc[:, 1::2]) 
-        return pos_enc
-    
-    def plot_table(self):
-        pos_enc_np = self.table.cpu().numpy()
-        plt.imshow(pos_enc_np, cmap='viridis', vmin=-1, vmax=1)
-        plt.colorbar()
-        plt.xlabel("Embedding index")
-        plt.ylabel("Sequence index")
-        plt.title('Sinusoidal Positional Encoding Table')
-        plt.show()
-
-    def forward(self, x):
-        return x + self.table[:,:x.size(2)]
+        return output,
 
 
 class Transformer(Module):
     def __init__(
-            self, d_src_vocab, d_tgt_vocab, dim_model: int = DIM_MODEL, n_layers: int = NUM_LAYERS, 
-            n_heads: int = NUM_HEADS, d_key: int = DIM_KEY, d_value: int = DIM_VALUE, 
-            dropout: float = DROPOUT, src_padding_idx: int = 0, tgt_padding_idx: int = 0, max_content: int = MAX_CONTEXT
+            self,
+            vocab_size: int = VOCAB_SIZE,
+            dim_model: int = DIM_MODEL,
+            n_layers: int = NUM_LAYERS, 
+            n_heads: int = NUM_HEADS,
+            d_head: int = DIM_HEAD,
+            dropout: float = DROPOUT,
+            padding_idx: int = 0,
+            max_content: int = MAX_CONTEXT
             ) -> None:
         super().__init__()
 
-        self.src_pad_idx, self.tgt_pad_idx = src_padding_idx, tgt_padding_idx
+        self.vocab_size = vocab_size
 
-        self.encoder = Encoder(d_src_vocab=d_src_vocab, dim_model=dim_model, n_layers=n_layers, 
-            n_heads=n_heads, d_key=d_key, d_value=d_value, dropout=dropout, padding_idx=src_padding_idx, max_content=max_content)
+        self.decoder = Decoder(
+            vocab_size=vocab_size,
+            dim_model=dim_model,
+            n_layers=n_layers,
+            n_heads=n_heads,
+            d_head=d_head,
+            dropout=dropout, 
+            padding_idx=padding_idx, 
+            max_content=max_content
+        )
         
-        self.decoder = Decoder(d_tgt_vocab=d_tgt_vocab, dim_model=dim_model, n_layers=n_layers, 
-            n_heads=n_heads, d_key=d_key, d_value=d_value, dropout=dropout, padding_idx=tgt_padding_idx, max_content=max_content)
-        
-        self.linear = Linear(dim_model, d_tgt_vocab, bias=False)
+        self.linear = Linear(dim_model, vocab_size, bias=False)
         self.softmax = nn.Softmax(dim=-1)
 
         for p in self.parameters():
             if p.dim() > 1:
                 nn.init.xavier_uniform_(p) 
 
-    def forward(self, src_seq, tgt_seq):
-        src_mask = self.get_pad_mask(src_seq, "src")
-        tgt_mask = self.get_pad_mask(tgt_seq, "tgt")
+    def forward(self, x):
+        mask = self.get_pad_mask(x)
 
-        enc_out, *_ = self.encoder(src_seq, src_mask=src_mask)
-        dec_out, *_ = self.decoder(seq=tgt_seq, enc_output=enc_out, src_mask=src_mask, tgt_mask=tgt_mask)
+        dec_out, *_ = self.decoder(x=x, mask=mask)
 
         return self.softmax(self.linear(dec_out))
     
-    def get_pad_mask(self, seq, mode: str = "src"):
-        assert mode == "src" or mode == "tgt"
+    def get_pad_mask(self, seq):
 
-        pad_idx = self.src_pad_idx if mode == "src" else self.tgt_pad_idx
+        pad_idx = self.src_pad_idx
         pad_mask = (seq != pad_idx).unsqueeze(-2)
 
-        if mode == "src":
-            return pad_mask
-        else:
-            _, len_s = seq.size()
-            subsequent_mask = (1 - torch.triu(
-                torch.ones((1, len_s, len_s), device=seq.device), diagonal=1)).bool()
-            return pad_mask & subsequent_mask
+        _, len_s = seq.size()
+        subsequent_mask = (1 - torch.triu(
+            torch.ones((1, len_s, len_s), device=seq.device), diagonal=1)).bool()
+        return pad_mask & subsequent_mask
