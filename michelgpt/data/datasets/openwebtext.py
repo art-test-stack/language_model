158d5f5e99e48e7b8cc80c16a92f110925a45626
Arthur Testard <testardarthur@gmail.com>
2024-08-04 23:24:57 +0200

    init openwebtext

---
+++ michelgpt/data/datasets/openwebtext.py
@@ -0,0 +1,12 @@
+from michelgpt.data.datasets.dataset import Dataset
+from michelgpt.settings import NUM_THREADS
+from datasets import load_dataset
+
+
+class OpenWebTextDataset(Dataset):
+    def __init__(self, multiplier: int | float = 1.) -> None:
+        super().__init__("openwebtext", multiplier)
+
+        dataset = load_dataset("openwebtext", num_proc=NUM_THREADS)
+
+
